9ce9abd9a24fabdfa1654030c126c791aea4e9b5
Hugh Huang <hhh7@pitt.edu>
2022-02-17 16:40:51 -0500

    changing header and recipe html components created recipe.model.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Chicken Parmesan', 'breaded chicken with parm on top', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202102-airfryerchickenparm-180-ls-1612561654.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*')
+    new Recipe('Chicken Parmesan', 'breaded chicken with parm on top',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202102-airfryerchickenparm-180-ls-1612561654.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*')
   ];
 
   constructor() { }
