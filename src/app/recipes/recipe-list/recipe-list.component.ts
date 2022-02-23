36b8d7dd044a90359c3d49b08be8f1aadb7db681
hhuang <hhuang@alliancepharmaco.com>
2022-02-22 23:54:54 -0500

    moved the recipeSelected logic to the service. We even emit the event that a recipe was selected from the service. But the recipe item component still calls the method

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
@@ -8,17 +8,12 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasClicked = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeClicked(recipe: Recipe) {
-    this.recipeWasClicked.emit(recipe);
   }
 
 }
